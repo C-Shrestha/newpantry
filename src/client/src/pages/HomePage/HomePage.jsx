8f73cdc4b90735f00f384bd8e9f150dd4e74c2c4
RuthShryock <ruthieshryock@gmail.com>
2022-10-28 15:06:03 -0400

    adding horizontal logo

--- src/client/src/pages/HomePage/HomePage.jsx
+++ src/client/src/pages/HomePage/HomePage.jsx
@@ -8,7 +8,7 @@ export default class LandingPage extends Component{
         return(
             <div className='landing'>
                 <div className="nav-bar">
-                    <img src="/Pantry.png" alt="pantry icon"/>
+                    <img src="/PantryHorizontal.png" alt="pantry icon"/>
                     <a href="/home" id="active">Home</a>
                     <a href="/favorites">Favorites</a>
                     <div className="search-bar">
