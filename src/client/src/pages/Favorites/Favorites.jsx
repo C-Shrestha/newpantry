7abdcdd9b5135dd83003618bf7fe29575bab92b2
Quinn Barber <qbarber2020@gmail.com>
2022-10-31 16:00:13 -0400

    Updated changes

--- src/client/src/pages/Favorites/Favorites.jsx
+++ src/client/src/pages/Favorites/Favorites.jsx
@@ -1,18 +1,32 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
 import React, {Component} from 'react';
 import './Favorites.css';
 
 
-export default class LandingPage extends Component{
+export default class FavoritesPage extends Component{
 
     render(){
         return(
             <div className='landing'>
                 <div className="nav-bar">
-                    <img src="/Pantry.png" alt="pantry icon"/>
+                    <img class ="pantry-pic" src="/PantryHorizontal.png" alt="pantry icon"/>
                     <a href="/home">Home</a>
                     <a href="/favorites" id="active">Favorites</a>
                     <div className="search-bar">
+                        <FontAwesomeIcon class="search-pic" icon={faMagnifyingGlass} />
                         <input></input>
+                    </div>
+                </div>
+                <div className="select">
+                    <div className="favorites">
+
+                    </div>
+                    <div className="pantry">
+
+                    </div>
+                    <div className="profile">
+                        
                     </div>
                 </div>
             </div>
--- src/client/src/pages/HomePage/HomePage.jsx
+++ src/client/src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,47 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
+import Carousel, { CarouselItem } from "../../components/Carousel";
 import React, {Component} from 'react';
 import './HomePage.css';
 
 
-export default class LandingPage extends Component{
+export default class HomePage extends Component{
 
     render(){
         return(
             <div className='landing'>
                 <div className="nav-bar">
-                    <img src="/PantryHorizontal.png" alt="pantry icon"/>
+                    <img class ="pantry-pic" src="/PantryHorizontal.png" alt="pantry icon"/>
                     <a href="/home" id="active">Home</a>
                     <a href="/favorites">Favorites</a>
                     <div className="search-bar">
+                        <FontAwesomeIcon class="search-pic" icon={faMagnifyingGlass} />
                         <input></input>
                     </div>
+                </div>
+                <div className="carousel">
+                    <Carousel>
+                        <CarouselItem>Beef</CarouselItem>
+                        <CarouselItem>Breakfast</CarouselItem>
+                        <CarouselItem>Chicken</CarouselItem>
+                        <CarouselItem>Dessert</CarouselItem>
+                        <CarouselItem>Goat</CarouselItem>
+                        <CarouselItem>Lamb</CarouselItem>
+                        <CarouselItem>Pasta</CarouselItem>
+                        <CarouselItem>Pork</CarouselItem>
+                        <CarouselItem>Seafood</CarouselItem>
+                        <CarouselItem>Side</CarouselItem>
+                        <CarouselItem>Starter</CarouselItem>
+                        <CarouselItem>Vegan</CarouselItem>
+                        <CarouselItem>Vegetarian</CarouselItem>
+                        <CarouselItem>Misc</CarouselItem>
+                    </Carousel>
+                </div>
+                <div className="seasonal">
+
+                </div>
+                <div className="trending">
+                    
                 </div>
             </div>
         );
