ad94d7795f6ea61c222ff0419fad6eb66822c154
Timothy Oei <timothyoei.dev@gmail.com>
2022-11-28 17:52:00 +0000

    Added crypto

--- src/server/routes/forgotPass.js
+++ src/server/routes/forgotPass.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+const crypto = require('crypto');
 const { sendRecoveryEmail } = require('../utils/emailUtils');
 
 router.get('/', async (req, res) => {
   await User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       user.recoveryToken = crypto.randomBytes(20).toString('hex');
-      user.save();
-      sendRecoveryEmail(req.body.email, user.recoveryToken);
+      console.log(user.recoveryToken);
+      user.save().then(() => {
+        sendRecoveryEmail(req.body.email, user.recoveryToken);
+      });
       res.status(200).send("Recovery email sent - please check your email");
+    } else {
+      res.status(404).send("Email not found");
     }
   }).catch((err) => {
     res.status(400).send(err);
+    console.log(err);
   });
 });
 
@@ -22,6 +29,8 @@ router.get('/:recoveryToken', async (req, res) => {
       user.password = req.params.recoveryToken;
       user.save();
       res.status(200).send(`Password changed to: ${req.params.recoveryToken} - please close this tab and login`);
+    } else {
+      res.status(404).send("Token not found");
     }
   }).catch((err) => {
     res.status(400).send(err);
