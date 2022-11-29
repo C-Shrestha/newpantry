4603a5195984d57e06866162dd6be02f4d619ab6
C-Shrestha <crason.shrestha@gmail.com>
2022-11-29 04:29:38 -0500

    added __v into userschema

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -43,8 +43,14 @@ const userSchema = new mongoose.Schema({
         type: Number,
         required: true,
         default: 0
+    }, 
+    __v: {
+        type: Number,
+        required: true,
+        default: 0, 
+        select: false
     }
 
-}, {collection: 'User'}, {timestamps: true}, {versionKey: false } );
+}, {collection: 'User'}, {timestamps: true}, {versionKey: false} );
 
 module.exports = mongoose.model('User', userSchema);
